c078f1817671f61e9e143ec64336f37d5103f4a2
Javier Gonzalez <jgromeo93@gmail.com>
2021-04-01 22:35:15 +0200

    first commit UI components, context and budget component

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem'
+const ExpenseList = () =>{
+    const expenses= [
+      { id: 1234, name: 'Shopping', cost: 50 },
+      { id: 1234, name: 'Holiday', cost: 300 },
+      { id: 1234, name: 'Transportation', cost: 70 },
+      { id: 1234, name: 'Fuel', cost: 40 },
+      { id: 1234, name: 'Child Care', cost: 500 }
+    ];
+
+    return(
+        <ul className="list-group">
+            {expenses.map((expense)=>(
+                <ExpenseItem 
+                id={expense.id} 
+                name={expense.name} 
+                cost={expense.cost}/>
+            ))}
+        </ul>
+    )
+    
+}
+
+export default ExpenseList;
---
+++ src/components/ExpenseTotal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ExpenseTotal = () => {
+    return (
+        <div className="alert alert-primary">
+            <span>
+                Spent so far: £1200
+            </span>
+        </div>
+    );
+}
+
+export default ExpenseTotal;
