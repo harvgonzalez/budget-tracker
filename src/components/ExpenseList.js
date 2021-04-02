44638b39c5cc55597ac66f51214874a849632664
Javier Gonzalez <jgromeo93@gmail.com>
2021-04-02 14:09:11 +0200

    delete and add actions added

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ExpenseItem from './ExpenseItem'
+import { AppContext } from '../context/AppContext';
+
 const ExpenseList = () =>{
-    const expenses= [
-      { id: 1234, name: 'Shopping', cost: 50 },
-      { id: 1234, name: 'Holiday', cost: 300 },
-      { id: 1234, name: 'Transportation', cost: 70 },
-      { id: 1234, name: 'Fuel', cost: 40 },
-      { id: 1234, name: 'Child Care', cost: 500 }
-    ];
+    const { expenses } = useContext(AppContext);
 
     return(
         <ul className="list-group">
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -2,18 +2,29 @@ import { createContext, useReducer } from 'react';
 
 const AppReducer = (state, action) => {
     switch(action.type){
+        case 'ADD_EXPENSE':
+            return {
+                ...state,
+                expenses: [...state.expenses, action.payload],
+            };
+        case 'DELETE_EXPENSE':
+            return {
+                ...state,
+                expenses: state.expenses.filter(
+                    (expense)=> expense.id !== action.payload
+                    ),
+            }
+
         default:
             return state;
     }
 }
 const initialState  = {
-    budget: 3000,
+    budget: 3500,
     expenses: [
-        { id: 12, name: 'shopping', cost: 40},
-        { id: 12, name: 'holiday', cost: 40},
-        { id: 12, name: 'shopping', cost: 40},
-        { id: 12, name: 'shopping', cost: 40},
-        { id: 12, name: 'shopping', cost: 40},
+        { id: 12, name: 'rent', cost: 1000},
+        { id: 13, name: 'utilities', cost: 250},
+        { id: 14, name: 'transportation', cost: 50},
     ]
 }
 export const AppContext = createContext();
